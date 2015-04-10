9bdf35528387c8b05cd77781f42f18dfb8e85811
guido <guidodorrego@hotmaiil.com>
2015-04-10 00:26:26 -0300

    se agregan las pantallas

---
+++ src/main/java/ungs/edu/ve/util/CONSTANTE.java
@@ -0,0 +1,14 @@
+package ungs.edu.ve.util;
+
+public interface CONSTANTE {
+
+	String ESTADO_INICIAL = "inicial";
+	String ESTADO_PROCESO = "proceso";
+	String ESTADO_FINALIZADO = "finalizado";
+	String MENSAJE_CONFIRMACION_VOTO = "Desea Confirmar su voto?";
+
+	Long CODIGO_INICIAL = 1L;
+	Long CODIGO_PROCESO = 2L;
+	Long CODIGO_FINALIZADO = 3L;
+
+}
---
+++ src/main/java/ungs/edu/ve/vista/PantallaVotanteSeleccionLista.java
@@ -0,0 +1,100 @@
+package ungs.edu.ve.vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javassist.tools.framedump;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+
+import java.awt.Checkbox;
+import java.awt.Button;
+
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class PantallaVotanteSeleccionLista extends JFrame {
+
+	private JPanel contentPane;
+	private static PantallaVotanteSeleccionLista frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame = new PantallaVotanteSeleccionLista();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PantallaVotanteSeleccionLista() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		String mensajeImportante = "Importante \n\nEl Sistema admite:\n\nVoto en Blanco, \nNo selecciona ninguna lista."
+				+ "\n\nVOTO IMPUGNADO, \n Seleccionando mas de dos listas \n\n Se permite la eleccion de una o hasta"
+				+ "dos listas como maximo";
+
+		JTextPane textPane = new JTextPane();
+		textPane.setText(mensajeImportante);
+		textPane.setBounds(12, 12, 126, 225);
+		contentPane.add(textPane);
+
+		Checkbox checkbox = new Checkbox("Lista 1");
+		checkbox.setBounds(170, 12, 115, 23);
+		contentPane.add(checkbox);
+
+		Checkbox checkbox_1 = new Checkbox("Lista 2");
+		checkbox_1.setBounds(170, 41, 115, 23);
+		contentPane.add(checkbox_1);
+
+		Checkbox checkbox_2 = new Checkbox("Lista 3");
+		checkbox_2.setBounds(170, 70, 115, 23);
+		contentPane.add(checkbox_2);
+
+		Checkbox checkbox_3 = new Checkbox("Lista 4");
+		checkbox_3.setBounds(170, 99, 115, 23);
+		contentPane.add(checkbox_3);
+
+		Checkbox checkbox_4 = new Checkbox("Lista 5");
+		checkbox_4.setBounds(170, 128, 115, 23);
+		contentPane.add(checkbox_4);
+
+		Checkbox checkbox_5 = new Checkbox("Lista 6");
+		checkbox_5.setBounds(170, 159, 115, 23);
+		contentPane.add(checkbox_5);
+
+		JButton btnVotar = new JButton("Votar");
+		btnVotar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				PopupConfirmacionVoto popupConfirmacionVoto = new PopupConfirmacionVoto();
+				popupConfirmacionVoto.setVisible(true);
+			}
+		});
+		btnVotar.setBounds(319, 223, 117, 25);
+		contentPane.add(btnVotar);
+	}
+
+	public static void cerrar() {
+		frame.setVisible(false);
+	}
+}
